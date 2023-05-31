3baa43001e6e0d64712e1cc651b5e1404338788f
FreekStelten <124879964+FreekStelten@users.noreply.github.com>
2023-05-31 10:19:40 +0200

    Yaml file + geen toegang bij geen reservering

--- main.go
+++ main.go
@@ -8,8 +8,17 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/go-yaml/yaml"
 	_ "github.com/microsoft/go-mssqldb"
 )
+
+type Config struct {
+	Server   string `yaml:"server"`
+	UserID   string `yaml:"user_id"`
+	Password string `yaml:"password"`
+	Port     string `yaml:"port"`
+	Database string `yaml:"database"`
+}
 
 type VehicleData struct {
 	Name         string
@@ -18,14 +27,38 @@ type VehicleData struct {
 	Einddatum    string
 }
 
+func loadConfig() (*Config, error) {
+	file, err := os.Open("config.yaml")
+	if err != nil {
+		return nil, fmt.Errorf("failed to open config file: %w", err)
+	}
+	defer file.Close()
+
+	var config Config
+	err = yaml.NewDecoder(file).Decode(&config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode config file: %w", err)
+	}
+
+	return &config, nil
+}
+
 func lookupHandler(w http.ResponseWriter, r *http.Request) {
 	// Get the licence plate value from the form submission
 	licencePlate := r.FormValue("licensePlate")
 
 	fmt.Println(licencePlate)
+
+	config, err := loadConfig()
+	if err != nil {
+		log.Println(err)
+		logToFile(err.Error()) // Log the error to the file
+		return
+	}
+
 	// Connection information for database
 	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%s;database=%s;",
-		"databasefonteyn.database.windows.net", "freek", "StudentStudent!", "1433", "Databasefonteyn")
+		config.Server, config.UserID, config.Password, config.Port, config.Database)
 
 	// Connect to the Azure database
 	db, err := sql.Open("sqlserver", connString)
@@ -34,7 +67,7 @@ func lookupHandler(w http.ResponseWriter, r *http.Request) {
 		logToFile(err.Error()) // Log the error to the file
 		return
 	}
-	defer db.Close() 
+	defer db.Close()
 
 	// Database query for selecting all of the reservation info.
 	selectLicencePlate, err := db.Query("SELECT Name, licenceplate, begindatum, Einddatum FROM slagboom_db WHERE licenceplate = @licenceplate", sql.Named("licencePlate", licencePlate))
@@ -65,6 +98,14 @@ func lookupHandler(w http.ResponseWriter, r *http.Request) {
 		logToFile(err.Error()) // Log the error to the file
 		return
 	}
+
+	if len(licencePlates) == 0 {
+		// Kenteken niet gevonden in de database
+		output := "U bent niet geregistreerd in ons park. Neem contact op met de balie voor verdere assistentie.\n"
+		io.WriteString(w, output)
+		return
+	}
+
 	fmt.Println(licencePlates)
 	output := "Hallo " + licenceplate.Name + ", welkom op fonteyn vakantieparken. Uw kentekenplaat is: " + licenceplate.Licenceplate + ". U heeft toegang van " + licenceplate.Startdatum + " tot " + licenceplate.Einddatum + ".\n" + "U kunt nu het park oprijden.\n"
 	io.WriteString(w, output)
