34e4307127707056ef43311a17d3bc78a0c0748e
FreekStelten <124879964+FreekStelten@users.noreply.github.com>
2023-06-01 11:40:21 +0200

    uitleg main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/go-yaml/yaml" //yamlfile voor config gegevens.
+	"github.com/go-yaml/yaml"           //yamlfile voor config gegevens.
 	_ "github.com/microsoft/go-mssqldb" //pakket wordt gebruikt om verbinding te maken met Microsoft SQL Server-databases vanuit een Go-programma.
 )
 
@@ -24,7 +24,7 @@ type Config struct {
 }
 
 // VehicleData bevat de gegevens van een voertuig
-//VehicleData structuur die de gegevens van een voertuig bevat, zoals naam, kentekenplaat, startdatum en einddatum.
+// VehicleData structuur die de gegevens van een voertuig bevat, zoals naam, kentekenplaat, startdatum en einddatum.
 type VehicleData struct {
 	Name         string
 	Licenceplate string
@@ -32,13 +32,12 @@ type VehicleData struct {
 	Einddatum    string
 }
 
-
-//Dit is de LoginForm structuur die het wachtwoordveld van het inlogformulier bevat.
+// Dit is de LoginForm structuur die het wachtwoordveld van het inlogformulier bevat.
 type LoginForm struct {
 	Password string
 }
 
-// De loadConfig functie opent "config.yaml" bestand en decodeert de inhoud naar een Config structuur. 
+// De loadConfig functie opent "config.yaml" bestand en decodeert de inhoud naar een Config structuur.
 // bij een fout optreedt bij het openen of decoderen van het bestand, wordt een fout geretourneerd.
 // Als er geen fouten optreden, wordt een verwijzing naar de Config structuur geretourneerd.
 func loadConfig() (*Config, error) {
@@ -58,9 +57,9 @@ func loadConfig() (*Config, error) {
 }
 
 // connectToDatabase maakt verbinding met de database
-// De connectToDatabase functie maakt verbinding met azure database met behulp van de opgegeven Config structuur. Het genereert een 
-// verbindingsreeks op basis van de configuratiegegevens en opent een databaseverbinding met behulp van de "sqlserver" driver. 
-//Als er een fout optreedt bij het verbinden met de database, wordt een fout geretourneerd. Anders wordt een verwijzing naar de sql.DB structuur geretourneerd.
+// De connectToDatabase functie maakt verbinding met azure database met behulp van de opgegeven Config structuur. Het genereert een
+// verbindingsreeks op basis van de configuratiegegevens en opent een databaseverbinding met behulp van de "sqlserver" driver.
+// Als er een fout optreedt bij het verbinden met de database, wordt een fout geretourneerd. Anders wordt een verwijzing naar de sql.DB structuur geretourneerd.
 func connectToDatabase(config *Config) (*sql.DB, error) {
 	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%s;database=%s;",
 		config.Server, config.UserID, config.Password, config.Port, config.Database)
@@ -73,13 +72,13 @@ func connectToDatabase(config *Config) (*sql.DB, error) {
 	return db, nil
 }
 
-// closeDatabase sluit hier de azure-databaseverbinding 
+// closeDatabase sluit hier de azure-databaseverbinding
 func closeDatabase(db *sql.DB) {
 	db.Close()
 }
 
-// De queryLicencePlate functie voert een query uit op de database om voertuiggegevens op te halen op basis van een kentekenplaat. 
-// Het bereidt de query voor met een parameter @licenceplate en voert vervolgens de query uit met de opgegeven kentekenplaatwaarde. 
+// De queryLicencePlate functie voert een query uit op de database om voertuiggegevens op te halen op basis van een kentekenplaat.
+// Het bereidt de query voor met een parameter @licenceplate en voert vervolgens de query uit met de opgegeven kentekenplaatwaarde.
 // Het scant de resultaten van de query in een slice van VehicleData en retourneert deze. Als er een fout optreedt tijdens het uitvoeren
 // van de query of het scannen van de resultaten, wordt een fout geretourneerd.
 func queryLicencePlate(db *sql.DB, licencePlate string) ([]VehicleData, error) {
@@ -105,8 +104,9 @@ func queryLicencePlate(db *sql.DB, licencePlate string) ([]VehicleData, error) {
 
 	return licencePlates, nil
 }
-// De loginHandler functie behandelt het inlogverzoek. Als de methode "GET" is, wordt het bestand "login.html" geserveerd. Als de 
-// methode "POST" is, wordt het ingevoerde wachtwoord gecontroleerd. Als het wachtwoord onjuist is, wordt de inlogstatus op false gezet 
+
+// De loginHandler functie behandelt het inlogverzoek. Als de methode "GET" is, wordt het bestand "login.html" geserveerd. Als de
+// methode "POST" is, wordt het ingevoerde wachtwoord gecontroleerd. Als het wachtwoord onjuist is, wordt de inlogstatus op false gezet
 // en een foutmelding naar de gebruiker gestuurd. Anders wordt de inlogstatus op true gezet en wordt de gebruiker omgeleid naar de hoofdpagina.
 func loginHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
@@ -127,13 +127,14 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/", http.StatusFound)
 	}
 }
+
 // Dit is een variabele die de inlogstatus bijhoudt.
 var loggedIn bool
 
 // lookupHandler behandelt het kentekenplaatzoekverzoek
 func lookupHandler(w http.ResponseWriter, r *http.Request) {
-//hier wordt gecontroleerd of de gebruiker is ingelogd. Als dat niet het geval is, wordt de gebruiker doorgestuurd 
-//naar de login-pagina. Vervolgens wordt de waarde van de licensePlate-parameter uit het verzoek gehaald.	
+	//hier wordt gecontroleerd of de gebruiker is ingelogd. Als dat niet het geval is, wordt de gebruiker doorgestuurd
+	//naar de login-pagina. Vervolgens wordt de waarde van de licensePlate-parameter uit het verzoek gehaald.
 	if loggedIn == false {
 		http.Redirect(w, r, "/login", http.StatusFound)
 		return
@@ -141,7 +142,7 @@ func lookupHandler(w http.ResponseWriter, r *http.Request) {
 
 	licencePlate := r.FormValue("licensePlate")
 
-	// Hier wordt de functie loadConfig aangeroepen om de configuratiegegevens te laden. 
+	// Hier wordt de functie loadConfig aangeroepen om de configuratiegegevens te laden.
 	// Als er een fout optreedt, wordt de fout gelogd en de functie gestopt.
 	config, err := loadConfig()
 	if err != nil {
@@ -150,8 +151,8 @@ func lookupHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// hier wordt de functie connectToDatabase aangeroepen om verbinding te maken met de azure database met behulp van de geladen 
-	// configuratiegegevens. Als er een fout optreedt, wordt de fout gelogd en de functie gestopt. De databaseverbinding wordt ook 
+	// hier wordt de functie connectToDatabase aangeroepen om verbinding te maken met de azure database met behulp van de geladen
+	// configuratiegegevens. Als er een fout optreedt, wordt de fout gelogd en de functie gestopt. De databaseverbinding wordt ook
 	// uitgesteld gesloten met behulp van defer om ervoor te zorgen dat de verbinding uiteindelijk wordt gesloten.
 	db, err := connectToDatabase(config)
 	if err != nil {
@@ -161,8 +162,8 @@ func lookupHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer closeDatabase(db)
 
-// Hier wordt de functie queryLicencePlate aangeroepen om de licentieplaten op te zoeken in de database met behulp van de verkregen 
-// databaseverbinding en de opgegeven kentekenplaat. Als er een fout optreedt, wordt de fout gelogd en de functie gestopt.
+	// Hier wordt de functie queryLicencePlate aangeroepen om de licentieplaten op te zoeken in de database met behulp van de verkregen
+	// databaseverbinding en de opgegeven kentekenplaat. Als er een fout optreedt, wordt de fout gelogd en de functie gestopt.
 	licencePlates, err := queryLicencePlate(db, licencePlate)
 	if err != nil {
 		log.Println(err)
@@ -170,8 +171,8 @@ func lookupHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Als er kenteken worden gevonden in de database, wordt er een Welkomsbericht samengesteld met de gegevens van de 
-	// eerste kentekenplaat en naar de HTTP-respons geschreven. Anders wordt er een andere boodschap geschreven om aan te geven dat 
+	// Als er kenteken worden gevonden in de database, wordt er een Welkomsbericht samengesteld met de gegevens van de
+	// eerste kentekenplaat en naar de HTTP-respons geschreven. Anders wordt er een andere boodschap geschreven om aan te geven dat
 	// de gebruiker niet geregistreerd is in het park. Vervolgens wordt de variabele loggedIn ingesteld op false. zodat ze opnieuw moeten inloggen voor het opnieuw invoeren.
 	if len(licencePlates) > 0 {
 		licenceplate := licencePlates[0]
@@ -183,8 +184,8 @@ func lookupHandler(w http.ResponseWriter, r *http.Request) {
 	loggedIn = false
 }
 
-// De functie logToFile wordt gebruikt om een foutbericht naar een bestand te loggen. Het opent een bestand genaamd "errors.txt" 
-// (of maakt het bestand als het nog niet bestaat) en schrijft de error naar het bestand. 
+// De functie logToFile wordt gebruikt om een foutbericht naar een bestand te loggen. Het opent een bestand genaamd "errors.txt"
+// (of maakt het bestand als het nog niet bestaat) en schrijft de error naar het bestand.
 // Als er een fout optreedt bij het openen van het bestand, wordt die error in terminal getoond.
 func logToFile(msg string) {
 	file, err := os.OpenFile("errors.txt", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
@@ -202,7 +203,8 @@ func logToFile(msg string) {
 func serveIndexPage(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "index.html")
 }
-//De main-functie is het startpunt van het programma. Het configureert de verschillende HTTP-handlers voor de verschillende routes ("/", "/lookup" en "/login").
+
+// De main-functie is het startpunt van het programma. Het configureert de verschillende HTTP-handlers voor de verschillende routes ("/", "/lookup" en "/login").
 // Het drukt ook een bericht af om aan te geven dat de server is gestart op "http://localhost:8080/login". Ten slotte start het de HTTP-server met behulp van
 // http.ListenAndServe en logt eventuele fouten die optreden tijdens het uitvoeren van de server.
 func main() {
